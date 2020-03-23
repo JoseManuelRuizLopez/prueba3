9ac68e702766b7ad82961f212840775114e6e7a2
JoseManuelRuizLopez <Josemanuelruizlopez97@gmail.com>
2020-03-23 12:08:27 +0100

    josema 2

--- app/src/main/java/com/example/prueba3/josemaac.java
+++ app/src/main/java/com/example/prueba3/josemaac.java
@@ -3,8 +3,11 @@ package com.example.prueba3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class josemaac extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +15,10 @@ public class josemaac extends AppCompatActivity {
         setContentView(R.layout.activity_josemaac);
 
         //hola esto saanno josema
+
+        btn = findViewById(R.id.button);
+
+        //sososos
+
     }
 }
