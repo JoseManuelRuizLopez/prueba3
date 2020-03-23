8b708f32c04969607f8e6d4a7800b1d6ce288c27
JoseManuelRuizLopez <Josemanuelruizlopez97@gmail.com>
2020-03-23 11:59:17 +0100

    josema 1

--- app/src/main/java/com/example/prueba3/josemaac.java
+++ app/src/main/java/com/example/prueba3/josemaac.java
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class josemaac extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_josemaac);
 
-
+        //hola esto saanno josema
     }
 }
