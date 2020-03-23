27c025f97e779d9569e50f782abec38289ffa69e
José Francisco Quintero Moreno <josequinteromoreno13@gmail.com>
2020-03-23 11:55:42 +0100

    cvommit

---
+++ app/src/main/java/com/example/prueba3/utyfgyt.java
@@ -0,0 +1,20 @@
+package com.example.prueba3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class utyfgyt extends AppCompatActivity {
+
+    Button btnBoton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_utyfgyt);
+
+        //Boton
+
+    }
+}
